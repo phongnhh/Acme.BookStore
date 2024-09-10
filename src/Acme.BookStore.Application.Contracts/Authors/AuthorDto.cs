3d2effb66af8032c41c80707d5b14aba94c2fc60
Phongnhh <phongnhh@onenet.vn>
2024-09-10 17:36:48 +0700

    Add Project

---
+++ src/Acme.BookStore.Application.Contracts/Authors/AuthorDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore.Authors;
+
+public class AuthorDto : EntityDto<Guid>
+{
+    public string Name { get; set; } = string.Empty;
+
+    public DateTime BirthDate { get; set; }
+
+    public string ShortBio { get; set; } = string.Empty;
+}
